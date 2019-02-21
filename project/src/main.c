148bc4680a0a24947ccc1d0ce9468b14b94f5801
piotr-kleins <piotr.kleins@gmail.com>
2019-02-21 16:24:37 +0100

    Initial commit

--- project/src/main.c
+++ project/src/main.c
@@ -1,17 +1,5 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
-
-
 #include "stm32f1xx.h"
-#include "stm32f1xx_nucleo.h"
-			
+
 
 int main(void)
 {
